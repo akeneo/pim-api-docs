e863dcccb5fa89afbb4c80b67944aa61f28c9972
Nolwenn Poirier <nolwenn.poirier@akeneo.com>
2021-01-15 11:04:08 +0100

    Too quick

--- tasks/copy-assets.js
+++ tasks/copy-assets.js
@@ -7,7 +7,7 @@ var merge = require('merge-stream');
 gulp.task('copy-assets', ['clean-dist'], function(){
     var fa = gulp.src('node_modules/font-awesome/css/font-awesome.min.css')
         .pipe(gulp.dest('dist/css/'));
-    var fonts = gulp.src(['node_modules/typeface-comfortaa/files/*',
+    var fonts = gulp.src(['node_modules/font-awesome/fonts/*',
               'node_modules/lato-font/fonts/lato-light/*',
               'node_modules/lato-font/fonts/lato-light-italic/*',
               'node_modules/lato-font/fonts/lato-normal/*',
