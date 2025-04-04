21340e2fbb87cbea56f5c62c54c734e19601023d
Loïc Fontaine <113339036+fontailo@users.noreply.github.com>
2025-04-04 14:14:37 +0200

    PIM-12026 [Security] Add SSL policy to use only TLS 1.2 (#1029)

--- deployments/modules/services/networking/load-balancer.tf
+++ deployments/modules/services/networking/load-balancer.tf
@@ -25,10 +25,17 @@ resource "google_compute_managed_ssl_certificate" "default" {
   }
 }
 
+resource "google_compute_ssl_policy" "default" {
+  name            = "default-ssl-tls-policy"
+  profile         = "COMPATIBLE"
+  min_tls_version = "TLS_1_2"
+}
+
 resource "google_compute_target_https_proxy" "default" {
   name             = "${local.context}-https-lb-proxy"
   url_map          = google_compute_url_map.default.id
   ssl_certificates = [google_compute_managed_ssl_certificate.default.id]
+  ssl_policy       = google_compute_ssl_policy.default.id
 }
 
 resource "google_compute_target_http_proxy" "https_redirect" {
